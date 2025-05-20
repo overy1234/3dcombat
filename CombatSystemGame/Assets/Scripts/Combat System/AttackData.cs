1fb82d7429f224e4f8d03f38b6922239c4522938
overy1234 <overy1234@naver.com>
2025-05-20 10:42:59 +0900

    combo

---
+++ CombatSystemGame/Assets/Scripts/Combat System/AttackData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Combat System/Create a new Attack")]
+public class AttackData : ScriptableObject
+{
+    [field:SerializeField] public string AnimName { get; private set; }
+    [field:SerializeField] public float ImpactStartTime { get; private set; }
+    [field:SerializeField] public float ImpactEndTime { get; private set; }
+}
