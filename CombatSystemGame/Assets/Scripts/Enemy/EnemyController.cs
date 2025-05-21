f625482a1be2ba176746ca360076f710f8a25582
overy1234 <overy1234@naver.com>
2025-05-21 18:03:19 +0900

    circling

--- CombatSystemGame/Assets/Scripts/Enemy/EnemyController.cs
+++ CombatSystemGame/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,20 +1,32 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
-public enum EnemyStates {  Idle, Chase}
+public enum EnemyStates {  Idle, CombatMovement}
 
 public class EnemyController : MonoBehaviour
 {
+    [field:SerializeField]public float Fov { get; private set; } = 180f;
+    public List<MeeleFighter> TargetsInRange { get; private set; } = new List<MeeleFighter>();
+    public MeeleFighter Target {  get;  set; }
+
     public StateMachine<EnemyController> StateMachine {  get; private set; }
 
     Dictionary<EnemyStates, State<EnemyController>> stateDict;
 
+    public NavMeshAgent NavAgent { get; private set; }
+
+    public Animator Anim {  get; private set; }
+
     private void Start()
     {
+        NavAgent = GetComponent<NavMeshAgent>();
+        Anim = GetComponent<Animator>();
+
         stateDict = new Dictionary<EnemyStates, State<EnemyController>>();
         stateDict[EnemyStates.Idle] = GetComponent<IdleState>();
-        stateDict[EnemyStates.Chase] = GetComponent<ChaseState>();
+        stateDict[EnemyStates.CombatMovement] = GetComponent<CombatMovementState>();
 
         StateMachine = new StateMachine<EnemyController>(this);
         StateMachine.ChangeState(stateDict[EnemyStates.Idle]);
@@ -28,5 +40,6 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         StateMachine.Execute();
+        Anim.SetFloat("moveAmount", NavAgent.velocity.magnitude / NavAgent.speed);
     }
 }
--- CombatSystemGame/Assets/Scripts/Enemy/IdleState.cs
+++ CombatSystemGame/Assets/Scripts/Enemy/IdleState.cs
@@ -6,20 +6,31 @@ public class IdleState : State<EnemyController>
     public override void Enter(EnemyController owner)
     {
         enemy = owner;
-        Debug.Log("아이들 Enter 스테이트");
+        
     }
 
     public override void Execute()
     {
-        Debug.Log("아이들 Execute 스테이트");
-        if (Input.GetKeyDown(KeyCode.T))
-            enemy.ChangeState(EnemyStates.Chase);
+        foreach(var target in enemy.TargetsInRange)
+        {
+            var vecToTarget = target.transform.position - transform.position;
+            float angle = Vector3.Angle(transform.forward, vecToTarget);
+
+            if(angle <= enemy.Fov /2)
+            {
+                enemy.Target = target;
+                enemy.ChangeState(EnemyStates.CombatMovement);
+                break;
+            }
+
+
+        }
 
     }
 
     public override void Exit()
     {
-        Debug.Log("아이들 Exit 스테이트");
+        
     }
 
 }
